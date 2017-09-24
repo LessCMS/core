5ad1a1a97d545b3a2a8682ca2a399fac330409b4
Jeremy Vaught <jeremy@jeremyvaught.com>
2017-09-24 04:28:51 -0400

    add some test routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,35 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::middleware('auth:api')->group(function () {
+
+	Route::get('/user', function (Request $request) {
+	    return $request->user();
+	});
+
+	Route::get('/resource', function (Request $request) {
+		// Do I need this? I don't want to call it a 'website', so what's a better name? And is this already known due to authentication/authorization?
+		return $request->resource();
+	});
+
+	Route::get('/post', function (Request $request) {
+		// I think I was to go with a better thing than 'post', thinking 'channel' is the right choice. Then everything is a channel. Pagest will also fit in channel, but perhaps having posts and pages as custom 'channel types'
+		return $request->post();
+	});
+
+	Route::get('/category', function (Request $request) {
+		return $request->category();
+	});
+
+	Route::get('/tag', function (Request $request) {
+		return $request->tag();
+	});
+
+
+
+
 });
